21202a7951cd4eda756d39daf69dcdc5103da4af
XueTang422 <Xue.Tang@energysage.com>
2023-03-28 14:56:10 -0400

    Set up bottom navigation and content fragments

--- app/src/main/java/com/example/esmobile/MainActivity.java
+++ app/src/main/java/com/example/esmobile/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.esmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.example.esmobile.Fragments.HomeFragment;
+import com.example.esmobile.Fragments.QuotingFragment;
+import com.example.esmobile.Fragments.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+//import android.databinding.DataBindingUtil;
+import com.example.esmobile.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.navigation_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.navigation_request_quote:
+                    replaceFragment(new QuotingFragment());
+                    break;
+                case R.id.navigation_setting:
+                    replaceFragment(new SettingFragment());
+                    break;
+            }
+            return true;
+        });
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home
+        ).build();
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
